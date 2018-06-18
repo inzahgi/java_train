e2de216326377858127c093304ef18cacd46c9b6
inzahgi <inzahgi@126.com>
2018-06-18 23:05:17 +0800

    add chapter2

---
+++ javaWebExample/chapter2/src/main/java/com/tcl/mie/service/CustomerService.java
@@ -0,0 +1,34 @@
+package com.tcl.mie.service;
+
+import com.tcl.mie.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomerService {
+
+    public List<Customer> getCustomerList(String keyword){
+        // TODO
+        return null;
+    }
+
+    public Customer getCustomer(long id){
+        // TODO
+        return null;
+    }
+
+    public boolean createCustomer(Map<String, Object> fieldMap){
+        // TODO
+        return false;
+    }
+
+    public boolean updateCustomer(long id, Map<String, Object> foeldMap){
+        // TODO
+        return false;
+    }
+
+    public boolean deleteCustomer(long id){
+        // TODO
+        return false;
+    }
+}
---
+++ javaWebExample/chapter2/src/main/resources/create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `customer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `contact` varchar(255) DEFAULT NULL,
+  `telephone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `remark` text,
+  PRIMARY key (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
