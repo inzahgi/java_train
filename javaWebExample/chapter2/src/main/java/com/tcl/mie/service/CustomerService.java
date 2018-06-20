1b690e012dfbc6f858a6aef64a81e4589f9836ee
inzahgi <inzahgi@126.com>
2018-06-20 23:29:37 +0800

    add dataUtil

--- javaWebExample/chapter2/src/main/java/com/tcl/mie/service/CustomerService.java
+++ javaWebExample/chapter2/src/main/java/com/tcl/mie/service/CustomerService.java
@@ -1,15 +1,31 @@
 package com.tcl.mie.service;
 
+import com.tcl.mie.helper.DatabaseHelper;
 import com.tcl.mie.model.Customer;
+import com.tcl.mie.util.PropsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class CustomerService {
 
-    public List<Customer> getCustomerList(String keyword){
-        // TODO
-        return null;
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
+
+
+
+    public List<Customer> getCustomerList(){
+        Connection conn = DatabaseHelper.getConnection();
+        try{
+            String sql="SELECT * FROM customer";
+            return DatabaseHelper.queryEntityList(Customer.class, conn, sql);
+        }finally {
+            DatabaseHelper.closeConnection(conn);
+        }
     }
 
     public Customer getCustomer(long id){
