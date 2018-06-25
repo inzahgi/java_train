ea32846aaad0ebaa79127ffbfbbd766d1af8a694
inzahgi <inzahgi@126.com>
2018-06-25 23:48:58 +0800

    add chapter4

---
+++ javaWebExample/chapter4/src/main/java/com/tcl/mie/service/CustomerService.java
@@ -0,0 +1,39 @@
+package com.tcl.mie.service;
+
+import com.tcl.mie.helper.DatabaseHelper;
+import com.tcl.mie.model.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomerService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
+
+
+
+    public List<Customer> getCustomerList(){
+        String sql = "SELECT * FROM customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
+    }
+
+    public Customer getCustomer(long id){
+        String sql = "SELECT * FROM customer WHERE id = ?";
+        return DatabaseHelper.queryEntity(Customer.class, sql, id);
+    }
+
+    public boolean createCustomer(Map<String, Object> fieldMap){
+        return DatabaseHelper.insertEntity(Customer.class, fieldMap);
+    }
+
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap){
+        return DatabaseHelper.updateEntity(Customer.class, id, fieldMap);
+    }
+
+    public boolean deleteCustomer(long id){
+        return DatabaseHelper.deleteEntity(Customer.class, id);
+    }
+}
