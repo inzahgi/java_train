f915350fe280261e43c6a1cf4f5303e90a1447eb
inzahgi <inzahgi@126.com>
2018-07-02 21:25:50 +0800

    add file upload

---
+++ javaWebExample/myFramework/src/main/java/com/tcl/mie/framework/helper/RequestHelper.java
@@ -0,0 +1,70 @@
+package com.tcl.mie.framework.helper;
+
+import com.tcl.mie.framework.bean.FormParam;
+import com.tcl.mie.framework.bean.Param;
+import com.tcl.mie.framework.util.ArrayUtil;
+import com.tcl.mie.framework.util.CodecUtil;
+import com.tcl.mie.framework.util.StreamUtil;
+import com.tcl.mie.framework.util.StringUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public final class RequestHelper {
+
+    public static Param createParam(HttpServletRequest request) throws IOException {
+        List<FormParam> formParamList = new ArrayList<FormParam>();
+        formParamList.addAll(parseParameterNames(request));
+        formParamList.addAll(parseInputStream(request));
+        return new Param(formParamList);
+    }
+
+    private static List<FormParam> parseParameterNames(HttpServletRequest request){
+        List<FormParam> formParamList = new ArrayList<FormParam>();
+        Enumeration<String> paramNames = request.getParameterNames();
+        while(paramNames.hasMoreElements()){
+            String fieldName = paramNames.nextElement();
+            String[] fieldValues = request.getParameterValues(fieldName);
+            if(ArrayUtil.isNotEmpty(fieldValues)){
+                Object fieldValue;
+                if(fieldValues.length == 1){
+                    fieldValue = fieldValues[0];
+                }else{
+                    StringBuilder sb = new StringBuilder("");
+                    for(int i = 0; i < fieldValues.length; i++){
+                        sb.append(fieldValues[i]);
+                        if(i != fieldValues.length - 1){
+                            sb.append(StringUtil.SEPARATOR);
+                        }
+                    }
+                    fieldValue = sb.toString();
+                }
+                formParamList.add(new FormParam(fieldName, fieldValue));
+            }
+        }
+        return formParamList;
+    }
+
+    private static List<FormParam> parseInputStream(HttpServletRequest request) throws IOException{
+        List<FormParam> formParamList = new ArrayList<FormParam>();
+        String body = CodecUtil.decodeURL(StreamUtil.getString(request.getInputStream()));
+        if(StringUtil.isNotEmpty(body)){
+            String[] kvs = StringUtil.splitString(body, "&");
+            if(ArrayUtil.isNotEmpty(kvs)){
+                for(String kv : kvs){
+                    String[] array = StringUtil.splitString(kv, "=");
+                    if(ArrayUtil.isNotEmpty(array) && array.length == 2){
+                        String fieldname = array[0];
+                        String fieldValue = array[1];
+                        formParamList.add(new FormParam(fieldname, fieldValue));
+                    }
+                }
+            }
+        }
+        return formParamList;
+    }
+
+}
